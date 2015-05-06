4d9b86ff65306f6ba342ade7e163605c4c08b012
lightbored <lightbored@new-host-8.home>
2015-05-06 08:32:25 -0400

    Initial Commit

---
+++ miniGolf/src/teamblc/minigolf/Course.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teamblc.minigolf;
+
+/**
+ *
+ * @author lightbored
+ */
+public class Course {
+    private int playerID;
+    private String playerName;
+    private String courseName;
+    private int[] score;
+
+/*
+String getName ()
+int getNumberOfHoles ()
+int parForHole ( int hole )
+*/    
+    
+}
---
+++ miniGolf/src/teamblc/minigolf/Round.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teamblc.minigolf;
+
+/**
+ *
+ * @author lightbored
+ */
+public class Round {
+    
+    private int playerID;
+    private String playerName;
+    private String courseName;
+    private int[] score;
+        
+    public void setPlayer ( int id, String name ) {
+       this.playerID = id;
+       this.playerName = name;
+    }
+
+    public void setCourse ( String name ) {
+       this.courseName = name;
+    }
+    
+    public String toString () {
+       return "Course: " + courseName + "\nPlayer Name: " + this.playerName;
+    }
+    
+    public int currentScore () { // Display the user's score so far (this is the number of strokes above or below par, for the holes played so far);
+        
+        return 0;
+    }
+    
+    public int numberOfHolesPlayed () { //Returns the number of completed holes in this round;
+        
+        return 0;
+    }    
+    
+    public synchronized void enterScore(int strokes) { //Enter a score for the next hole.  (Note the current design doesn't allow a player to skip holes, they must be played in order);
+      // some thread critical stuff
+      // here
+    } 
+    
+    public int[] scoreDetail() {
+        
+        return this.score;
+    }
+}
