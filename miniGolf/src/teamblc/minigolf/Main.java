96d538f55ebf1321a01825e3ac4ad3ba2ca91750
lightbored <lightbored@new-host-8.home>
2015-05-06 08:52:48 -0400

    Added stubs

--- miniGolf/src/teamblc/minigolf/Main.java
+++ miniGolf/src/teamblc/minigolf/Main.java
@@ -10,25 +10,38 @@ package teamblc.minigolf;
  * @author lightbored
  */
 public class Main {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
     }
+
+    
+    static Round newRound ( String courseName, String playerName ){ //This method creates and stores a new round object, and returns a reference to that Round object.
+        
+        return roundData;
+    }
+        
+    static Round getRound (String player, String courseName ) { //This method looks up the Round object.  Note it isn't specified how you store Round objects, but a likely choice is some sort of “Collection”.  Keep in mind the garbage collection requirements for this object, discussed above.
+        
+    }
+
+    static Course getCourse ( String courseName ) { //Returns a Course object.  These must be persisted to storage but there aren't many courses so creating a “Collection” to hold them all, read from a file say, is reasonable.  However exactly how you do this is an implementation detail.
+        
+    }
+
+    static int scoreSoFar( String courseName, String playerName ) { //This method looks up the round information and returns it's score so far to the swipe station, to display.  (Yes it is redundant with Round.currentScore(), but the customer insisted — probably has a nephew or niece who is a “computer expert”.)
+        
+    return 0;
+    }
+
+    static void enterStrokes ( String courseName, String playerName, int strokes ) { //This method is redundant.  It also will be used by the swipe-station once a player hits the button for their number of strokes for that hole.
+    
+    }
+
     
     /*
-    static Round newRound ( String courseName, String playerName ) 
-This method creates and stores a new round object, and returns a reference to that Round object.
-static Round getRound (String player, String courseName ) 
-This method looks up the Round object.  Note it isn't specified how you store Round objects, but a likely choice is some sort of “Collection”.  Keep in mind the garbage collection requirements for this object, discussed above.
-static Course getCourse ( String courseName ) 
-Returns a Course object.  These must be persisted to storage but there aren't many courses so creating a “Collection” to hold them all, read from a file say, is reasonable.  However exactly how you do this is an implementation detail.
-static int scoreSoFar( String courseName, String playerName ) 
-This method looks up the round information and returns it's score so far to the swipe station, to display.  (Yes it is redundant with Round.currentScore(), but the customer insisted — probably has a nephew or niece who is a “computer expert”.)
-static void enterStrokes ( String courseName, String playerName, int strokes ) 
-This method is redundant.  It also will be used by the swipe-station once a player hits the button for their number of strokes for that hole.
 Getters for the properties: course list, round list.
     */
     
